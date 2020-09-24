787acf241f804a774e24aec1bdf9862ed6f46df9
wuyp <wuyp@fsmeeting.com>
2020-09-24 19:07:00 +0800

    first commit

---
+++ 23，Module的语法/profile.js
@@ -0,0 +1,4 @@
+export var firstName = 'wu';
+export var lastName = 'yaping';
+export var year = 2020;
+export var month = '08';
---
+++ 25，编程风格/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wuyp on 2019/11/25.
+ */
+const a = 'foobar';
+const b = `foo${a}bar`;
+console.log(b);// 'foofoobarbar'
